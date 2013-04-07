5f1676d30a17933897a42fe13a42ad4932dc9905
Jonathan Dallas <github@jonathandallas.com>
2013-04-07 19:36:43 -0400

    initial creation of site and database

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Knobblossom2::Application.config.secret_token = '8862fd982b572bcc03b730b097ca8b5d7b84b5a98a8764c0f8467619cedbcb9cf7305d210341bcc1aad5fbd7f42949e2c7d518344ccd47c65c8a537091cc7dc8'
